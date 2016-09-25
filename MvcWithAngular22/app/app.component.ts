8fd84012144f31690cb4dc0326ef6f6b0df404ae
Vivek Sharma <sharmavk76@gmail.com>
2016-09-24 22:19:03 -0500

    Add
    Add

---
+++ MvcWithAngular22/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Events App</h1><my-events></my-events>'
+})
+export class AppComponent { }
---
+++ MvcWithAngular22/app/event.component.ts
@@ -0,0 +1,14 @@
+﻿import {Component} from '@angular/core';
+@Component({
+    selector: 'my-events',
+    template: `
+            <h2>{{title}}</h2>
+            <ul><li *ngFor="let event of events">{{event}}</li></ul>
+        `
+})
+export class EventsComponent {
+    title: string = "Events List";
+
+    events: string[] = ['event 1', 'event 2', 'event 3', 'event 4'];
+
+}
